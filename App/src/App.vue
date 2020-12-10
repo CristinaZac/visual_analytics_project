383e92712e13ff37d8a16aa216a2e7e807732187
CristinaZac <cristina.zaccagnino@gmail.com>
2020-12-10 13:05:07 +0100

    Project creation

---
+++ App/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
