61856129e433c145a2ed656f7b97214953655530
CristinaZac <cristina.zaccagnino@gmail.com>
2020-12-14 19:45:24 +0100

    added new element

--- App/src/App.vue
+++ App/src/App.vue
@@ -2,8 +2,26 @@
   <div id="app">
     <b-container class="bv-example-row">
       <b-row>
-        <b-col>1 of 3</b-col>
-        <b-col>2 of 3</b-col>
+        <b-col>
+          <b-form-group>
+            <h4>Select a date</h4>
+            <b-form-checkbox-group
+              v-model="days.value"
+              :options="days.options"
+              name="buttons-1"
+              buttons
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <b-form-group>
+            <h4>Select a neighborhood</h4>
+            <b-form-select v-model="location.selected" :options="location.options"></b-form-select>
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <h4>Select a timeframe</h4>
+              <b-form-timepicker v-model="time.valuestart" locale="en"></b-form-timepicker>
+              <b-form-timepicker v-model="time.valueend" locale="en"></b-form-timepicker>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -16,8 +34,6 @@ import crossfilter from 'crossfilter';
 let cf;
 let dTime; // orario
 let dDate; // data
-// eslint-disable-next-line camelcase
-let dDate_h; // data e ora
 let dlocation;
 
 export default {
@@ -27,6 +43,21 @@ export default {
   data() {
     return {
       reports: [],
+      reportFilter: [],
+
+      days: {
+        options: [],
+        value: String,
+      },
+      time: {
+        options: [],
+        valuestart: '00:00:00',
+        valueend: '12:00:00',
+      },
+      location: {
+        selected: String,
+        options: [],
+      },
     };
   },
   mounted() {
@@ -44,23 +75,58 @@ export default {
           medical: d.medical,
           buildings: d.buildings,
           shake_intensity: d.shake_intensity,
-
         }));
-      });
-    cf = crossfilter(this.reports);
-    dDate = cf.dimension(d => d.date);
-    dTime = cf.dimension(d => d.time);
-    // eslint-disable-next-line camelcase
-    dDate_h = cf.dimension(d => d.date_h);
-    dlocation = cf.dimension(d => d.location);
-    /* eslint-disable */
-    console.log('time', dTime.group().reduceCount().all());
-    console.log('date', dDate.group().reduceCount().all());
-    console.log('date_h', dDate_h.group().reduceCount().all());
-    console.log('location', dlocation.group().reduceCount().all());
+        cf = crossfilter(this.reports);
+        dDate = cf.dimension(d => d.date);
+        dTime = cf.dimension(d => d.time);
+        // eslint-disable-next-line camelcase
+        dlocation = cf.dimension(d => d.location);
+        dDate.group().reduceCount().all();
+        dTime.group().reduceCount().all();
+        dlocation.group().reduceCount().all();
 
-   // this.date.options = dDate.group().reduceCount().all().map(v => v.key);
-   // this.date.value = this.date.options[0];
+        // this.time.options = dDate_h.group().reduceCount().all().map(v => v.key);
+        // this.time.value = this.time.options[0];
+        this.days.options = dDate
+          .group().reduceCount().all().map(v => v.key);
+        this.days.value = this.days.options[0];
+        this.location.options = dlocation
+          .group().reduceCount().all().map(v => v.key);
+        this.location.selected = this.location.options[0];
+      });
+  },
+
+  methods: {
+    refreshTime() {
+      this.reportFilter = this.reports.filter(value =>
+        value.date === this.days.value &&
+        value.time >= this.time.valuestart &&
+        value.time <= this.time.valueend &&
+        value.location === this.location.selected);
+    },
+  },
+  watch: {
+    time: {
+      handler(newVal) {
+        dTime.filter(newVal.value);
+        this.refreshTime();
+      },
+      deep: true,
+    },
+    days: {
+      handler(newVal) {
+        dDate.filter(newVal.value);
+        this.refreshTime();
+      },
+      deep: true,
+    },
+    location: {
+      handler(newVal) {
+        dlocation.filter(newVal.value);
+        this.refreshTime();
+      },
+      deep: true,
+    },
   },
 };
 
